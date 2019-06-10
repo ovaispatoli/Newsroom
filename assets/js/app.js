6894882fd8fc187e82badc913c97803213fd375d
OvaisPatoli <ovaispatoli@gmail.com>
2019-06-10 09:02:38 -0500

    finished 'makeResponsive' function

--- assets/js/app.js
+++ assets/js/app.js
@@ -59,10 +59,87 @@ function makeResponsive() {
 
         var xAxis = d3.axisBottom(xScale);
         var yAxis = d3.axisLeft(yScale);
-    })
+
+        // Append axes
+        
+        // x-axis
+        chartGroup.append("g")
+            .attr("transform", `translate(0, ${height}`)
+            .call(xAxis);
+        // y-axis
+        chartGroup.append("g")
+            .call(yAxis);
 
 
-}
+        // append circles
+        chartGroup.selectAll("circle")
+            .data(fullData)
+            .enter()
+            .append("circle")
+            .attr("cx", d => xScale(d.age))
+            .attr("cy", d => yScale(d.smokes))
+            .attr("r", "10")
+            .attr("fill", "blue")
+            .attr("stroke-width", "1")
+            .attr("stroke", "black")
+            .attr("opacity", "0.5")
+            .on("mouseover", function(){
+                d3.select(this)
+                    .attr("fill", "red");
+            })
+            .on("mouseout", function(){
+                d3.select(this)
+                    .attr("fill", "blue");
+            });
 
+
+        // append text for chart (circles)
+        chartGroup.append("g").selectAll("text")
+            .data(fullData)
+            .enter()
+            .append("text")
+            .text(d => d.abbr)
+            .attr("x", d => xScale(d.age))
+            .attr("y", d => yScale (d.smokes))
+            .attr("text-anchor", "middle")
+            .attr("alignment-baseline", "central")
+            .attr("font_family", "sans-serif")
+            .attr("font-size", "10px")
+            .attr("fill", "white")
+            .attr("font-weight", "bold");
+
+        // append text for axes
+
+        // x-axis
+        chartGroup.append("text")
+            .attr("transform", `translate(${width}/2, ${height + margin.top - 10})`)
+            .attr("text-anchor", "middle")
+            .attr("font-size", "15px")
+            .attr("fill", "black")
+            .attr ("font_family", "sans-serif")
+            .text("Age")
+
+        // y-axis
+        chartGroup.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - (margin.left /2))
+            .attr("x", 0 - (height/2))
+            .attr("text-anchor", "middle")
+            .attr("font-size", "15px")
+            .attr("fill", "black")
+            .attr ("font_family", "sans-serif")
+            .text("Percentage of Smokers");
+    
+            
+
+
+    });
+
+
+};
+
+makeResponsive();
+
+d3.select(window).on("resize", makeResponsive);
 
 
