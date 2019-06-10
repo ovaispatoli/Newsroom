05a0102fe7267d14d48fd88182cd61aa065bcfb8
OvaisPatoli <ovaispatoli@gmail.com>
2019-06-10 07:24:04 -0500

    started working on 'makeResponsive' function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,52 @@
 // @TODO: YOUR CODE HERE!
+
+function makeResponsive() {
+   
+    // Check to see if svgArea is empty or not, if it is not replace and remove the area with chart
+    var svgArea = d3.select("#scatter").select("svg");
+
+    if (!svgArea.empty()){
+        svgArea.remove();
+    }
+
+    // Parameters for SVG
+    var svgHeight = window.innerHeight;
+    var svgWidth = window.innerWidth;
+
+    // Margins
+    var margin = {
+        top: 50,
+        right: 50, 
+        bottom: 50,
+        left: 50
+    };
+
+    // Chart area subracted by margins
+    var chartHeight = svgHeight - margin.top - margin.bottom;
+    var chartWidth = svgWidth - margin.left - margin.right;
+
+    // Make SVG container
+    var svg = d3.select("#scatter").append("svg")
+        .attr("height", svgHeight)
+        .attr("width", svgWidth);
+
+    // shift everything over to margins
+    var chartGroup = svg.append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // Get data from CSV file
+
+    d3.csv("assets/data/data.csv").then((data)=>{
+        // console.log(data);
+
+        // Loop over to find smoker data and append for each smoker and their age
+        data.forEach((data)=>{
+            data.smokes 
+        })
+    })
+
+
+}
+
+
+
